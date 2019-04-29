42b49c1f3fd7c783acc5b5f28a8b63d6bc7445b4
Marcel Märtens <marcel.cochem@googlemail.com>
2019-04-30 01:10:33 +0200

    start working on RFC#11, implement a regionmanager and basic region component as well as tests for those this is a fist implementation and likly to change
    Former-commit-id: fdb7097dc30ab1642d25a02532458bcc2811ab61

--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -6,6 +6,13 @@ use std::time::Duration;
 use tracing::{info, Level};
 use tracing_subscriber::{filter::LevelFilter, EnvFilter, FmtSubscriber};
 
+use std::sync::{mpsc, Arc};
+use worldsim::{
+    regionmanager::{RegionManager, meta::RegionManagerMsg},
+    server::meta::{ServerMsg},
+    job::JobManager,
+    region::Region,
+};
 const TPS: u64 = 30;
 const RUST_LOG_ENV: &str = "RUST_LOG";
 
@@ -44,6 +51,17 @@ fn main() {
     let settings = ServerSettings::load();
     let server_port = &settings.gameserver_address.port();
     let metrics_port = &settings.metrics_address.port();
+
+    let (region_manager_tx, region_manager_rx) = mpsc::channel::<RegionManagerMsg>();
+    let (server_tx, server_rx) = mpsc::channel::<ServerMsg>();
+
+    let mut region_manager = RegionManager::new(region_manager_tx, server_rx);
+    let mut job_manager: Arc<JobManager> = Arc::new(JobManager::new());
+    let mut server = worldsim::server::Server::new(server_tx,region_manager_rx,job_manager.clone());
+    let mut region = Region::new((0,0),job_manager.clone());
+
+    job_manager.repeat(move || region_manager.work() );
+    job_manager.repeat(move || server.work() );
 
     // Create server
     let mut server = Server::new(settings).expect("Failed to create server instance!");
