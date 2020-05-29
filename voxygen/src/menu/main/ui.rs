ec46a1c656133ae4cc1355c85e5de48248eb6407
Imbris <imbrisf@gmail.com>
2020-05-29 00:40:22 -0400

    Add support for TextInput widget, rename widgets module to widget, update to glyph_brush 0.7.0

--- voxygen/src/menu/main/ui.rs
+++ voxygen/src/menu/main/ui.rs
@@ -224,6 +224,7 @@ impl IcedState {
             .press_image(self.imgs.button_press)
             .text_color(TEXT_COLOR)
             .disabled_text_color(DISABLED_TEXT_COLOR);
+
         let buttons = Column::with_children(vec![
             self.servers_button.view(
                 i18n.get("common.servers"),
@@ -424,7 +425,7 @@ impl MainMenuUi {
             .unwrap()
             .read_to_end(&mut buf)
             .unwrap();
-            glyph_brush::rusttype::Font::from_bytes(buf).unwrap()
+            ui::ice::Font::try_from_vec(buf).unwrap()
         };
 
         let mut ice_ui = IcedUi::new(window, ice_font).unwrap();
